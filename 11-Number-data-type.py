dea78672fed4535626a6766c7373a94775f3d8a4
omar52 <omarrabdoo00@gmail.com>
2025-04-28 05:27:54 +0300

    3rd commit

---
+++ 11-Number-data-type.py
@@ -0,0 +1,5 @@
+# complex number consisted of [real + imag] can be written in two ways:
+# 1- complex = 2 + 3j
+# 2- num = complex(2,3) , it returns float number
+
+num = complex(2,3); print(num, int(num.imag))
---
+++ 12-built-in_function.py
@@ -0,0 +1,2 @@
+print(abs(-5.6))  #return absolute value
+print(round(5.4028,3)) # Round a number to a given precision in decimal digits.
